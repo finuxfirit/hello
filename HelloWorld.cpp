7989a8275805f787e1638f2c8c285dd010996c43
finuxfirit <foxcorpofficial@gmail.com>
2022-07-28 16:00:56 -0500

    add files

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // variables
+    int favoriteNumber = 69;
+    float PI = 3.141592653589793238462643383279502884197169; 
+
+    cout << "================================================================================" << endl;
+    cout << "Hello World!" << endl;
+    cout << "I am a text blog that ends with a slash-n instead of a endl\n";
+    cout << "My favorite number is: " << favoriteNumber << endl;
+    cout << "My favorite floating number is PI which is: " << PI << endl;
+    cout << "================================================================================" << endl;
+
+    return 0;
+}
